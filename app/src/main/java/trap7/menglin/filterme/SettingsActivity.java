c614518732440d693ea22a08b6d6294eb93002c1
pyInvenio <forrestm.a113@gmail.com>
2019-12-04 14:12:46 -0500

    bruh

--- app/src/main/java/trap7/menglin/filterme/SettingsActivity.java
+++ app/src/main/java/trap7/menglin/filterme/SettingsActivity.java
@@ -1,14 +1,31 @@
 package trap7.menglin.filterme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class SettingsActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }
+        mAdapter = new MyAdapter(input);
+        recyclerView.setAdapter(mAdapter);
     }
 }
